56ef2294e551826b8a459bc93bbc53b795353b0e
Matheus Hunsche <matheusarendthunsche@gmail.com>
2018-09-23 05:55:29 -0300

    added published initial

---
+++ cmd/publish.go
@@ -0,0 +1,32 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/hashload/boss/models"
+	"github.com/spf13/cobra"
+)
+
+func publish(p *models.Package) {
+	fmt.Println(p.Homepage)
+}
+
+var publishCmd = &cobra.Command{
+	Use:   "publish",
+	Short: "publish a dependency",
+	Long:  `publish a dependency`,
+	Run: func(cmd *cobra.Command, args []string) {
+		loadPackage, e := models.LoadPackage("package.json")
+		if e != nil {
+			e.Error()
+		}
+
+		publish(loadPackage)
+
+		loadPackage.Save()
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(publishCmd)
+}
--- cmd/root.go
+++ cmd/root.go
@@ -11,19 +11,12 @@ import (
 
 var cfgFile string
 
-// RootCmd represents the base command when called without any subcommands
 var RootCmd = &cobra.Command{
-	Use:   "cobra-example",
-	Short: "An example of cobra",
-	Long: `This application shows how to create modern CLI 
-applications in go using Cobra CLI library`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	//	Run: func(cmd *cobra.Command, args []string) { },
+	Use:   "boss",
+	Short: "Dependency Manager for Delphi",
+	Long:  "Dependency Manager for Delphi",
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	if err := RootCmd.Execute(); err != nil {
 		fmt.Println(err)
@@ -33,38 +26,23 @@ func Execute() {
 
 func init() {
 	cobra.OnInitialize(initConfig)
-
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-	RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.cobra-example.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	RootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
-// initConfig reads in config file and ENV variables if set.
 func initConfig() {
 	if cfgFile != "" {
-		// Use config file from the flag.
 		viper.SetConfigFile(cfgFile)
 	} else {
-		// Find home directory.
 		home, err := homedir.Dir()
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(1)
 		}
 
-		// Search config in home directory with name ".cobra-example" (without extension).
 		viper.AddConfigPath(home)
-		viper.SetConfigName(".cobra-example")
 	}
 
-	viper.AutomaticEnv() // read in environment variables that match
+	viper.AutomaticEnv()
 
-	// If a config file is found, read it in.
 	if err := viper.ReadInConfig(); err == nil {
 		fmt.Println("Using config file:", viper.ConfigFileUsed())
 	}
