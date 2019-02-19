5e327e4cf03c50bf3a6b6d25e23caa9a0103eee1
Rodrigo Bernardi <coloradonanet@unochapeco.edu.br>
2019-02-18 22:19:26 -0300

    up version

--- consts/init.go
+++ consts/init.go
@@ -10,4 +10,4 @@ const XML_TAG_NAME_PROPERTY_ATTRIBUTE_VALUE string = "'$(Base)'!=''"
 
 const XML_TAG_NAME_LIBRARY_PATH string = "DCC_UnitSearchPath"
 
-const VERSION string = "v1.5.0"
+const VERSION string = "v1.5.3"
