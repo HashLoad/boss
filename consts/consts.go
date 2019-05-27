b740e200734ad8d9deaded2eb175b815be2b4f0c
Rodrigo Bernardi <coloradonanet@unochapeco.edu.br>
2019-05-27 17:27:25 -0300

    Adjusted path generation for boss mudule compiler

--- consts/consts.go
+++ consts/consts.go
@@ -25,7 +25,7 @@ const XmlTagNamePropertyAttributeValue string = "'$(Base)'!=''"
 
 const XmlTagNameLibraryPath string = "DCC_UnitSearchPath"
 
-const Version string = "v2.5.5"
+const Version string = "v2.5.6"
 
 const BossInternalDir = "{internal}"
 
--- installer/boss.iss
+++ installer/boss.iss
@@ -1,5 +1,5 @@
 #define BossName "Boss"
-#define BossVersion "2.5.5"
+#define BossVersion "2.5.6"
 #define BossPublisher "Hashload"
 #define BossURL "https://github.com/HashLoad/boss"
 
