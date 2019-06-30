2c0f53bd0bd23ab1ebe4dadb42bee17333d39819
viniciussanchez <viniciuss.sanchez@gmail.com>
2019-06-30 15:28:45 -0300

    Updated short and long messages

--- cmd/install.go
+++ cmd/install.go
@@ -7,8 +7,8 @@ import (
 
 var installCmd = &cobra.Command{
 	Use:     "install",
-	Short:   "Install a dependency",
-	Long:    `Install a dependency`,
+	Short:   "Install a new dependency",
+	Long:    `This command install a new dependency`,
 	Aliases: []string{"i", "add"},
 	Run: func(cmd *cobra.Command, args []string) {
 		core.InstallModules(args, true)
