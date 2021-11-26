f6049f1da6cc3f31a6161b0b347ea8bf05b3c444
Vinicius Sanchez <viniciuss.sanchez@gmail.com>
2021-11-26 14:32:52 -0300

    Code improvements

--- cmd/init.go
+++ cmd/init.go
@@ -1,7 +1,15 @@
 package cmd
 
 import (
-	"github.com/hashload/boss/core"
+	"bufio"
+	"fmt"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
+
+	"github.com/hashload/boss/env"
+	"github.com/hashload/boss/models"
 	"github.com/spf13/cobra"
 )
 
@@ -17,11 +25,69 @@ var initCmd = &cobra.Command{
   Initialize a new project without having it ask any questions:
   boss init --quiet`,
 	Run: func(cmd *cobra.Command, args []string) {
-		core.InitializeBossPackage(quiet)
+		doInitialization(quiet)
 	},
 }
 
 func init() {
 	RootCmd.AddCommand(initCmd)
 	initCmd.Flags().BoolVarP(&quiet, "quiet", "q", false, "without asking questions")
+}
+
+func doInitialization(quiet bool) {
+	if !quiet {
+		printHead()
+	}
+
+	pkgJson, _ := models.LoadPackage(true)
+
+	var folderName = ""
+	rxp, err := regexp.Compile(`^.+\` + string(filepath.Separator) + `([^\\]+)$`)
+	if err == nil {
+		allString := rxp.FindAllStringSubmatch(env.GetCurrentDir(), -1)
+		folderName = allString[0][1]
+	}
+
+	if quiet {
+		pkgJson.Name = folderName
+		pkgJson.Version = "1.0.0"
+		pkgJson.MainSrc = "./"
+	} else {
+		pkgJson.Name = getParamOrDef("Package name ("+folderName+")", folderName)
+		pkgJson.Homepage = getParamOrDef("Homepage", "")
+		pkgJson.Version = getParamOrDef("Version (1.0.0)", "1.0.0")
+		pkgJson.Description = getParamOrDef("Description", "")
+		pkgJson.MainSrc = getParamOrDef("Source folder (./src)", "./src")
+	}
+
+	json := pkgJson.Save()
+	fmt.Println("\n" + string([]byte(json)))
+}
+
+func getParamOrDef(msg string, def string) string {
+	fmt.Print(msg + ": ")
+	rr := bufio.NewReader(os.Stdin)
+	if res, e := rr.ReadString('\n'); e == nil && res != "\n" {
+		res = strings.ReplaceAll(res, "\t", "")
+		res = strings.ReplaceAll(res, "\n", "")
+		res = strings.ReplaceAll(res, "\r", "")
+		if res == "" {
+			return def
+		} else {
+			return res
+		}
+	}
+	return def
+}
+
+func printHead() {
+	println(`
+This utility will walk you through creating a boss.json file.
+It only covers the most common items, and tries to guess sensible defaults.
+		 
+Use 'boss install <pkg>' afterwards to install a package and
+save it as a dependency in the boss.json file.
+
+Press ^C at any time to quit.
+`)
 }
--- cmd/login.go
+++ cmd/login.go
@@ -1,8 +1,16 @@
 package cmd
 
 import (
-	"github.com/hashload/boss/core"
+	"fmt"
+	"log"
+	"os/user"
+	"path/filepath"
+	"syscall"
+
+	"github.com/hashload/boss/env"
+	"github.com/hashload/boss/msg"
 	"github.com/spf13/cobra"
+	"golang.org/x/crypto/ssh/terminal"
 )
 
 var removeLogin bool
@@ -14,7 +22,7 @@ var loginCmd = &cobra.Command{
   boss login <repo>`,
 	Aliases: []string{"adduser", "add-user"},
 	Run: func(cmd *cobra.Command, args []string) {
-		core.Login(removeLogin, args)
+		login(removeLogin, args)
 	},
 }
 
@@ -22,4 +30,65 @@ func init() {
 	// TODO add example to remove login or add a new command to logout (equals branch refact-steroids)
 	loginCmd.Flags().BoolVarP(&removeLogin, "rm", "r", false, "remove login")
 	RootCmd.AddCommand(loginCmd)
+}
+
+func login(removeLogin bool, args []string) {
+	configuration := env.GlobalConfiguration
+
+	if removeLogin {
+		delete(configuration.Auth, args[0])
+		configuration.SaveConfiguration()
+		return
+	}
+
+	var auth *env.Auth
+	var repo string
+	if len(args) > 0 && args[0] != "" {
+		repo = args[0]
+		auth = configuration.Auth[args[0]]
+	} else {
+		repo = getParamOrDef("Url to login (ex: github.com)", "")
+		if repo == "" {
+			msg.Die("Empty is not valid!!")
+		}
+		auth = configuration.Auth[repo]
+	}
+
+	if auth == nil {
+		auth = &env.Auth{}
+	}
+
+	auth.UseSsh = getParamBoolean("Use SSH")
+	if auth.UseSsh {
+		auth.Path = getParamOrDef("Path of ssh private key("+getSshKeyPath()+")", getSshKeyPath())
+	} else {
+		auth.SetUser(getParamOrDef("Username", ""))
+		auth.SetPass(getPass("Password"))
+	}
+	configuration.Auth[repo] = auth
+	configuration.SaveConfiguration()
+
+}
+
+func getPass(description string) string {
+	fmt.Print(description + ": ")
+
+	bytePassword, err := terminal.ReadPassword(int(syscall.Stdin))
+	if err != nil {
+		msg.Die("Error on get pass: %s", err)
+	}
+	return string(bytePassword)
+}
+
+func getSshKeyPath() string {
+	usr, e := user.Current()
+	if e != nil {
+		log.Fatal(e)
+	}
+	return filepath.Join(usr.HomeDir, ".ssh", "id_rsa")
+}
+
+func getParamBoolean(msgS string) bool {
+	msg.Print(msgS + "(y or n): ")
+	return msg.PromptUntilYorN()
 }
