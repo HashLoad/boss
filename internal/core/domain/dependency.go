5bac7e0e6513bcf1e524deb44c7a935a3c760181
CarlosHe <caiqi_04@hotmail.com>
2025-12-13 05:42:31 -0300

    :recycle: refactor: consolidate regular expressions for improved readability and maintainability

--- internal/core/domain/dependency.go
+++ internal/core/domain/dependency.go
@@ -13,6 +13,15 @@ import (
 	"github.com/hashload/boss/pkg/msg"
 )
 
+var (
+	reSSHUrl            = regexp.MustCompile(`(?m)([\w\d.]*)(?:/)(.*)`)
+	reURLPrefix         = regexp.MustCompile(`^[^/^:]+`)
+	reHasHTTPS          = regexp.MustCompile(`(?m)^https?:\/\/`)
+	reVersionMajorMinor = regexp.MustCompile(`(?m)^(.|)(\d+)\.(\d+)$`)
+	reVersionMajor      = regexp.MustCompile(`(?m)^(.|)(\d+)$`)
+	reDepName           = regexp.MustCompile(`[^/]+(:?/$|$)`)
+)
+
 type Dependency struct {
 	Repository string
 	version    string
@@ -37,16 +46,14 @@ func (p *Dependency) SSHUrl() string {
 	if strings.Contains(p.Repository, "@") {
 		return p.Repository
 	}
-	re = regexp.MustCompile(`(?m)([\w\d.]*)(?:/)(.*)`)
-	submatch := re.FindStringSubmatch(p.Repository)
+	submatch := reSSHUrl.FindStringSubmatch(p.Repository)
 	provider := submatch[1]
 	repo := submatch[2]
 	return "git@" + provider + ":" + repo
 }
 
 func (p *Dependency) GetURLPrefix() string {
-	urlPrefixPattern := regexp.MustCompile(`^[^/^:]+`)
-	return urlPrefixPattern.FindString(p.Repository)
+	return reURLPrefix.FindString(p.Repository)
 }
 
 func (p *Dependency) GetURL() string {
@@ -60,29 +67,25 @@ func (p *Dependency) GetURL() string {
 	if p.UseSSH {
 		return p.SSHUrl()
 	}
-	var hasHTTPS = regexp.MustCompile(`(?m)^https?:\/\/`)
-	if hasHTTPS.MatchString(p.Repository) {
+	if reHasHTTPS.MatchString(p.Repository) {
 		return p.Repository
 	}
 
 	return "https://" + p.Repository
 }
 
-var re = regexp.MustCompile(`(?m)^(.|)(\d+)\.(\d+)$`)
-var re2 = regexp.MustCompile(`(?m)^(.|)(\d+)$`)
-
 func ParseDependency(repo string, info string) Dependency {
 	parsed := strings.Split(info, ":")
 	dependency := Dependency{}
 	dependency.Repository = repo
 	dependency.version = parsed[0]
-	if re.MatchString(dependency.version) {
-		msg.Warn("Current version for %s is not semantic (x.y.z), for comparison using %s -> %s",
+	if reVersionMajorMinor.MatchString(dependency.version) {
+		msg.Debug("Current version for %s is not semantic (x.y.z), for comparison using %s -> %s",
 			dependency.Repository, dependency.version, dependency.version+".0")
 		dependency.version += ".0"
 	}
-	if re2.MatchString(dependency.version) {
-		msg.Warn("Current version for %s is not semantic (x.y.z), for comparison using %s -> %s",
+	if reVersionMajor.MatchString(dependency.version) {
+		msg.Debug("Current version for %s is not semantic (x.y.z), for comparison using %s -> %s",
 			dependency.Repository, dependency.version, dependency.version+".0.0")
 		dependency.version += ".0.0"
 	}
@@ -109,8 +112,7 @@ func GetDependenciesNames(deps []Dependency) []string {
 }
 
 func (p *Dependency) Name() string {
-	var re = regexp.MustCompile(`[^/]+(:?/$|$)`)
-	return re.FindString(p.Repository)
+	return reDepName.FindString(p.Repository)
 }
 
 // GetKey returns the normalized key for the dependency (lowercase repository).
